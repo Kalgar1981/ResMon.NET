e8b805debcb1a112d73185caf15aa19f2cdbc2c7
kalgar1981 <kalgar1981@hotmail.com>
2020-01-08 20:06:46 +0100

    Added Network graph. CPU load graphs not fixed anymore. One per logical core shall be displayed.

--- Form1.vb
+++ Form1.vb
@@ -1,56 +1,66 @@
 ﻿Imports System.ComponentModel
 
 Public Class frmMain
+
+    Private loading As Boolean = False
+    Private isDown As Boolean = False
+    Private lastLocation As Point
+
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        loading = True
+        Height = 40
         lblComName.Text = GetCompName()
         InitializeCharts()
 
         bkwCPU.RunWorkerAsync()
-
+        loading = False
     End Sub
 
     Private Sub InitializeCharts()
+        Dim perfCat As PerformanceCounterCategory = New PerformanceCounterCategory("Network Interface")
+        Dim instance As String = perfCat.GetInstanceNames()(0)
 
-        InitializeChart(chtCPU1)
-        InitializeChart(chtCPU2)
-        InitializeChart(chtCPU3)
-        InitializeChart(chtCPU4)
-        InitializeChart(chtCPU5)
-        InitializeChart(chtCPU6)
-        InitializeChart(chtCPU7)
-        InitializeChart(chtCPU8)
+        Dim coreCount As Integer = Environment.ProcessorCount
 
+        For c = 0 To coreCount - 1
+            lstCounters.Add(New PerformanceCounter("Processor", "% Processor Time", c.ToString))
+            Dim cht As New DataVisualization.Charting.Chart
+            cht.Name = c.ToString
+            InitializeChart(cht, "CPU" & c.ToString, c)
+            lstCharts.Add(cht)
+            lstValues.Add(0)
+
+            Controls.Add(lstCharts(c))
+
+        Next
+
+        Dim chtNet As New DataVisualization.Charting.Chart
+        chtNet.Name = "Net"
+        InitializeChart(chtNet, "NET")
+        lstCharts.Add(chtNet)
+
+        lstValues.Add(0)
+        lstValues.Add(0)
+
+        lstCounters.Add(New PerformanceCounter("Network Interface", "Bytes Sent/sec", instance))
+        lstCounters.Add(New PerformanceCounter("Network Interface", "Bytes Received/sec", instance))
+
+        Controls.Add(chtNet)
 
     End Sub
 
 
-
     Private Sub UpdateLabels()
-        If chtCPU1.InvokeRequired Then
-            chtCPU1.Invoke(Sub() UpdateLabels())
+        If lblComName.InvokeRequired Then
+            lblComName.Invoke(Sub() UpdateLabels())
         Else
             SuspendLayout()
 
-            updatechart(chtCPU1, pc1Value)
-            updatechart(chtCPU2, pc2Value)
-            updatechart(chtCPU3, pc3Value)
-            updatechart(chtCPU4, pc4Value)
-            updatechart(chtCPU5, pc5Value)
-            updatechart(chtCPU6, pc6Value)
-            updatechart(chtCPU7, pc7Value)
-            updatechart(chtCPU8, pc8Value)
+            For c = 0 To lstCharts.Count - 3
+                updatechart(lstCharts(c), lstValues(c))
+            Next
 
-
-
-            lblCPU1Value.Text = pc1Value.ToString & "%"
-            lblCPU2Value.Text = pc2Value.ToString & "%"
-            lblCPU3Value.Text = pc3Value.ToString & "%"
-            lblCPU4Value.Text = pc4Value.ToString & "%"
-            lblCPU5Value.Text = pc5Value.ToString & "%"
-            lblCPU6Value.Text = pc6Value.ToString & "%"
-            lblCPU7Value.Text = pc7Value.ToString & "%"
-            lblCPU8Value.Text = pc8Value.ToString & "%"
-
+            updateNetChart(lstCharts.Last, lstValues(lstValues.Count - 2), lstValues(lstValues.Count - 1))
             ResumeLayout()
         End If
 
@@ -65,7 +75,29 @@ Public Class frmMain
 
     End Sub
 
+    Private Sub frmMain_SizeChanged(sender As Object, e As EventArgs) Handles Me.SizeChanged
+        If loading Then Exit Sub
+        For c = 0 To lstCharts.Count - 1
+            lstCharts(c).Size = New Size(Width - 4, chartHeight)
+            lstCharts(c).Location = New Point(2, 30 + (lstCharts(c).Size.Height + 4) * c)
+        Next
+    End Sub
 
+    Private Sub MDown(sender As Object, e As MouseEventArgs) Handles lblComName.MouseDown
+        isDown = True
+        lastLocation = e.Location
+    End Sub
+
+    Private Sub MUp(sender As Object, e As MouseEventArgs) Handles lblComName.MouseUp
+        isDown = False
+    End Sub
+
+    Private Sub MMove(sender As Object, e As MouseEventArgs) Handles lblComName.MouseMove
+        If isDown Then
+            Location = New Point((Location.X - lastLocation.X) + e.X, (Location.Y - lastLocation.Y) + e.Y)
+            Update()
+        End If
+    End Sub
 End Class
 
 
--- ModMain.vb
+++ ModMain.vb
@@ -1,62 +1,155 @@
 ﻿Module ModMain
 
-    Public pc1 As New PerformanceCounter("Processor", "% Processor Time", "0")
-    Public pc2 As New PerformanceCounter("Processor", "% Processor Time", "1")
-    Public pc3 As New PerformanceCounter("Processor", "% Processor Time", "2")
-    Public pc4 As New PerformanceCounter("Processor", "% Processor Time", "3")
-    Public pc5 As New PerformanceCounter("Processor", "% Processor Time", "4")
-    Public pc6 As New PerformanceCounter("Processor", "% Processor Time", "5")
-    Public pc7 As New PerformanceCounter("Processor", "% Processor Time", "6")
-    Public pc8 As New PerformanceCounter("Processor", "% Processor Time", "7")
+    Public lstCharts As New List(Of DataVisualization.Charting.Chart)
+    Public lstCounters As New List(Of PerformanceCounter)
+    Public lstValues As New List(Of Integer)
 
-    Public pc1Value As Integer
-    Public pc2Value As Integer
-    Public pc3Value As Integer
-    Public pc4Value As Integer
-    Public pc5Value As Integer
-    Public pc6Value As Integer
-    Public pc7Value As Integer
-    Public pc8Value As Integer
+    Public lstSentValues As New List(Of Integer)
+    Public lstReceivedValues As New List(Of Integer)
+
+
 
     Public updatesPerSecond As Integer = 1
+    Public chartHeight As Integer = 60
 
-    Public cpu1Points As New List(Of DataVisualization.Charting.DataPoint)
+    Public lastLocation As Point
 
     Public Function GetCompName() As String
         Return Environment.MachineName
     End Function
 
-    Public Sub InitializeChart(ByRef cht As DataVisualization.Charting.Chart)
+    Public Sub InitializeChart(ByRef cht As DataVisualization.Charting.Chart, ByVal chartName As String)
 
-        cht.ChartAreas.Add("CPU1")
-        cht.Series.Add("CPU1")
-        cht.Series(0).ChartArea = cht.ChartAreas(0).Name
+        cht.Size = New Size(frmMain.Width - 4, chartHeight)
+        cht.Location = New Point(2, lastLocation.Y + (cht.Size.Height + 8))
+
+        frmMain.Height += chartHeight + 8
+
+        cht.BackColor = Color.FromKnownColor(KnownColor.Transparent)
+        cht.BorderlineColor = Color.LightGray
+
+        cht.ChartAreas.Add(chartName)
+        cht.Series.Add("Sent")
+        cht.Series.Add("Received")
+
+        cht.Series(0).ChartArea = chartName
+
+        cht.ChartAreas(0).AlignmentOrientation = DataVisualization.Charting.AreaAlignmentOrientations.Horizontal
+        cht.ChartAreas(0).Position.Width = 100
+        cht.ChartAreas(0).Position.Height = 100
+        cht.ChartAreas(0).BorderWidth = 0
+
+        'Sent bytes/second
         cht.Series(0).ChartType = DataVisualization.Charting.SeriesChartType.Column
+        cht.Series(0).CustomProperties = "PointWidth=1"
+        cht.Series(0).CustomProperties = "DrawSideBySide=False"
+        cht.Series(0).Color = Color.FromArgb(50, Color.Red)
 
-        cht.ChartAreas(0).BackColor = Color.FromKnownColor(KnownColor.Transparent)
+        'Receibed bytes/second
+        cht.Series(1).ChartType = DataVisualization.Charting.SeriesChartType.Column
+        cht.Series(1).CustomProperties = "PointWidth=1"
+        cht.Series(1).CustomProperties = "DrawSideBySide=False"
+        cht.Series(1).Color = Color.FromArgb(50, Color.Blue)
+
+        cht.ChartAreas(0).BackColor = Color.LightGray
         cht.ChartAreas(0).AxisX.LabelStyle.Enabled = False
+
+        cht.ChartAreas(0).AxisX.IntervalAutoMode = DataVisualization.Charting.IntervalAutoMode.FixedCount
+        cht.ChartAreas(0).AxisX.MajorTickMark.Enabled = False
         cht.ChartAreas(0).AxisX.MajorGrid.LineWidth = 0
-        cht.ChartAreas(0).AxisX.MinorGrid.LineWidth = 0
-        cht.ChartAreas(0).AxisX.MinorTickMark.Enabled = False
+        cht.ChartAreas(0).AxisX.LineWidth = 0
+
         cht.ChartAreas(0).AxisY.LabelStyle.Enabled = False
+        cht.ChartAreas(0).AxisY.MinorGrid.LineWidth = 0
+        cht.ChartAreas(0).AxisY.IntervalAutoMode = DataVisualization.Charting.IntervalAutoMode.VariableCount
+        cht.ChartAreas(0).AxisY.Interval = 25
+
+        For i = 0 To 99
+            cht.Series(0).Points.AddXY(i, 0)
+            cht.Series(1).Points.AddXY(i, 0)
+        Next
+
+    End Sub
+
+    Public Sub InitializeChart(ByRef cht As DataVisualization.Charting.Chart, ByVal chartName As String, ByVal chtIndex As Integer)
+
+        cht.Size = New Size(frmMain.Width - 4, chartHeight)
+        cht.Location = New Point(2, 30 + (cht.Size.Height + 8) * chtIndex)
+        lastLocation = cht.location
+
+        frmMain.Height += chartHeight + 8
+
+        cht.BackColor = Color.FromKnownColor(KnownColor.Transparent)
+        cht.BorderlineColor = Color.LightGray
+
+        cht.ChartAreas.Add(chartName)
+        cht.Series.Add(chartName)
+        cht.Series(0).ChartArea = chartName
+
+        cht.ChartAreas(0).AlignmentOrientation = DataVisualization.Charting.AreaAlignmentOrientations.Horizontal
+        cht.ChartAreas(0).Position.Width = 100
+        cht.ChartAreas(0).Position.Height = 100
+        cht.ChartAreas(0).BorderWidth = 0
+
+        cht.Series(0).ChartType = DataVisualization.Charting.SeriesChartType.Column
+        cht.Series(0).CustomProperties = "PointWidth=1"
+        cht.ChartAreas(0).BackColor = Color.LightGray
+
+        cht.ChartAreas(0).AxisX.LabelStyle.Enabled = False
+
+        cht.ChartAreas(0).AxisX.IntervalAutoMode = DataVisualization.Charting.IntervalAutoMode.FixedCount
+        cht.ChartAreas(0).AxisX.MajorTickMark.Enabled = False
+        cht.ChartAreas(0).AxisX.MajorGrid.LineWidth = 0
+        cht.ChartAreas(0).AxisX.LineWidth = 0
+
+        cht.ChartAreas(0).AxisY.LabelStyle.Enabled = False
+        cht.ChartAreas(0).AxisY.MinorGrid.LineWidth = 0
         cht.ChartAreas(0).AxisY.Minimum = 0
         cht.ChartAreas(0).AxisY.Maximum = 100
+        cht.ChartAreas(0).AxisY.IntervalAutoMode = DataVisualization.Charting.IntervalAutoMode.FixedCount
+        cht.ChartAreas(0).AxisY.Interval = 25
 
         For i = 0 To 99
             cht.Series(0).Points.AddXY(i, 0)
         Next
+
     End Sub
 
     Public Sub updatechart(ByRef cht As DataVisualization.Charting.Chart, ByVal value As Integer)
+
         cht.Series(0).Points.SuspendUpdates()
 
-        cht.Series(0).Points.AddY(value)
+            cht.Series(0).Points.AddY(value)
+            cht.Series(0).Points.RemoveAt(0)
+            For i = 0 To cht.Series(0).Points.Count - 1
+                cht.Series(0).Points(i).XValue = i
+                cht.Series(0).Points(i).Color = getColor(value)
+            Next
+
+            cht.Series(0).Points.ResumeUpdates()
+
+
+    End Sub
+
+    Public Sub updateNetChart(ByRef cht As DataVisualization.Charting.Chart, ByVal sentValue As Integer, ByVal receivedValue As Integer)
+        cht.Series(0).Points.SuspendUpdates()
+        cht.Series(1).Points.SuspendUpdates()
+
+        cht.Series(0).Points.AddY(sentValue)
         cht.Series(0).Points.RemoveAt(0)
+
+        cht.Series(1).Points.AddY(receivedValue)
+        cht.Series(1).Points.RemoveAt(0)
+
         For i = 0 To cht.Series(0).Points.Count - 1
             cht.Series(0).Points(i).XValue = i
         Next
+        For i = 0 To cht.Series(1).Points.Count - 1
+            cht.Series(1).Points(i).XValue = i
+        Next
 
         cht.Series(0).Points.ResumeUpdates()
+        cht.Series(1).Points.ResumeUpdates()
     End Sub
 
 
@@ -76,15 +169,14 @@
 
     Public Sub UpdateCounters()
 
-        pc1Value = CInt(pc1.NextValue)
-        pc2Value = CInt(pc2.NextValue)
-        pc3Value = CInt(pc3.NextValue)
-        pc4Value = CInt(pc4.NextValue)
-        pc5Value = CInt(pc5.NextValue)
-        pc6Value = CInt(pc6.NextValue)
-        pc7Value = CInt(pc7.NextValue)
-        pc8Value = CInt(pc8.NextValue)
-
+        For c = 0 To lstCounters.Count - 1
+            Dim counter As PerformanceCounter = DirectCast(lstCounters(c), PerformanceCounter)
+            If c < lstCounters.Count - 2 Then
+                lstValues(c) = CInt(counter.NextValue)
+            Else
+                lstValues(c) = CInt(counter.NextValue / 1024)
+            End If
+        Next
 
     End Sub
 
